30dca0ae116a44e1727b0415afdd58f18767feff
jirkov17 <147742590+jirkov17@users.noreply.github.com>
2025-04-15 02:19:05 +0300

    Update Comands.cpp

--- Comands.cpp
+++ Comands.cpp
@@ -1,12 +1,18 @@
 #include "commands.hpp"
 #include <iostream>
+#include <functional>
+#include <algorithm>
 #include <fstream>
-#include "list.hpp"
+#include <vector>
 
-using dictionary = BST< std::string, std::string >;
+using dictionary = std::multimap< std::string, std::string >;
 
-void jirkov::help(std::ostream& output)
+void jirkov::help(std::istream& input, std::ostream& output)
 {
+  if (input.peek() != '\n')
+  {
+    throw std::exception();
+  }
   output << "1) help - output of available commands\n";
   output << "2) create <name> - create a dictionary named name\n";
   output << "3) remove <name> - delete a dictionary named name\n";
@@ -22,7 +28,7 @@ void jirkov::help(std::ostream& output)
   output << "13) save - shut down a file, exit edit module, save data\n";
 }
 
-void jirkov::createDict(BST< std::string, dictionary >& dicts, std::istream& input, std::ostream& output)
+void jirkov::createDict(std::map< std::string, dictionary >& dicts, std::istream& input, std::ostream& output)
 {
   std::string dictName;
   input >> dictName;
@@ -30,38 +36,41 @@ void jirkov::createDict(BST< std::string, dictionary >& dicts, std::istream& inp
   {
     return;
   }
-  dicts.insert(dictName, dictionary());
-  dictionary currentDict = dicts.at(dictName).second;
+  dicts.emplace(dictName, dictionary());
+  dictionary currentDict = dicts[dictName];
   using namespace std::placeholders;
-  BST< std::string, std::function< void(dictionary&, std::istream&, std::ostream&) > > cmdDict;
-  cmdDict.insert("delete", std::bind(jirkov::deleteKey, _1, _2, _3));
-  cmdDict.insert("find", std::bind(jirkov::findKey, _1, _2, _3));
-  cmdDict.insert("open", std::bind(jirkov::openFile, _1, _2, _3));
+  std::map< std::string, std::function< void(dictionary&, std::istream&, std::ostream&) > > cmdDict;
+  {
+    cmdDict["delete"] = std::bind(jirkov::deleteKey, _1, _2, _3);
+    cmdDict["find"] = std::bind(jirkov::findKey, _1, _2, _3);
+    cmdDict["open"] = std::bind(jirkov::openFile, _1, _2, _3);
+  }
+  auto warningMes = std::bind(warning, std::placeholders::_1, "Dictionary does not exist\n");
   std::string cmdType;
   while (input >> cmdType && cmdType != "stop")
   {
-    if (cmdDict.find(cmdType) != cmdDict.cend())
+    try
     {
-      cmdDict.at(cmdType).second(currentDict, input, output);
-      dicts.erase(dictName);
-      dicts.insert(dictName, currentDict);
+      cmdDict.at(cmdType)(currentDict, input, output);
+      dicts[dictName] = currentDict;
     }
-    else
+    catch (const std::out_of_range& e)
     {
-      warningDict(output);
-      input.clear();
-      input.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      warningMes(output);
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
 }
 
 void jirkov::deleteKey(dictionary& dict, std::istream& input, std::ostream& output)
 {
+  auto warningMes = std::bind(warning, std::placeholders::_1, "Element does not exist\n");
   std::string key;
   input >> key;
-  if (dict.find(key) == dict.cend())
+  if (dict.find(key) == dict.end())
   {
-    warningElem(output);
+    warningMes(output);
     return;
   }
   dict.erase(key);
@@ -69,35 +78,43 @@ void jirkov::deleteKey(dictionary& dict, std::istream& input, std::ostream& outp
 
 void jirkov::findKey(dictionary& dict, std::istream& input, std::ostream& output)
 {
+  auto warningMes = std::bind(warning, std::placeholders::_1, "Element does not exist\n");
   std::string key;
   input >> key;
-  if (dict.find(key) == dict.cend() || dict.find(key)->second.empty())
+  if (dict.find(key) == dict.end() || dict.find(key)->second.empty())
   {
-    warningElem(output);
+    warningMes(output);
     return;
   }
-  output << dict.find(key)->second << "\n";
+  output << dict.find(key)->second;
 }
 
 void jirkov::openFile(dictionary& dict, std::istream& input, std::ostream& output)
 {
+  auto warningMes = std::bind(warning, std::placeholders::_1, "File does not exist\n");
   std::string word;
-  List< std::string > words;
+  std::vector< std::string > words;
   std::string filename;
   input >> filename;
   std::ifstream in(filename);
   if (!in)
   {
-    warningFile(output);
+    warningMes(output);
     return;
   }
   while (in >> word)
   {
-    std::string lowercaseWord = "";
-    for (char c : word)
-    {
-      lowercaseWord += std::tolower(c);
-    }
+    std::string lowercaseWord = word;
+    std::transform
+    (
+      word.begin(),
+      word.end(),
+      lowercaseWord.begin(),
+      [](char c)
+      {
+        return std::tolower(c);
+      }
+    );
     std::string result = "";
     for (char c : lowercaseWord)
     {
@@ -106,44 +123,44 @@ void jirkov::openFile(dictionary& dict, std::istream& input, std::ostream& outpu
         result += c;
       }
     }
-    words.pushBack(result);
+    words.push_back(result);
   }
-  for (List< std::string >::Iterator it = words.begin(); it != List< std::string >::ConstIterator(); it++)
+  for (auto it = words.begin(); it != words.end(); it++)
   {
-    size_t frequency = 0;
-    for (List< std::string >::Iterator sIt = words.begin(); sIt != List< std::string >::ConstIterator(); sIt++)
+    size_t frequency = std::count
+    (
+      words.begin(),
+      words.end(),
+      *it
+    );
+    if (dict.find(*it) == dict.end())
     {
-      if (*sIt == *it)
-      {
-        frequency++;
-      }
-    }
-    if (dict.find(*it) == dict.cend())
-    {
-      dict.insert(*it, std::to_string(frequency));
+      dict.emplace(*it, std::to_string(frequency));
     }
   }
 }
 
-void jirkov::writeToFile(const BST< std::string, dictionary >& dicts, std::istream& in, std::ostream& out)
+void jirkov::writeToFile(const std::map< std::string, dictionary >& dicts, std::istream& in, std::ostream& out)
 {
+  auto warningMes = std::bind(warning, std::placeholders::_1, "File does not exist\n");
   std::string filename;
   in >> filename;
   std::ofstream output;
   output.open(filename, std::ios::app);
   if (!output)
   {
-    warningFile(out);
+    warningMes(out);
     return;
   }
+  auto warningMesDict = std::bind(warning, std::placeholders::_1, "Dictionary does not exist\n");
   std::string dictName;
   in >> dictName;
-  if (dicts.constFind(dictName) == dicts.cend())
+  if (dicts.find(dictName) == dicts.end())
   {
-    warningDict(out);
+    warningMesDict(out);
     return;
   }
-  dictionary dict = dicts.constFind(dictName)->second;
+  dictionary dict = dicts.find(dictName)->second;
   output << "\n" << dictName << "\n";
   for (auto it = dict.begin(); it != dict.end(); it++)
   {
@@ -152,30 +169,32 @@ void jirkov::writeToFile(const BST< std::string, dictionary >& dicts, std::istre
   output.close();
 }
 
-void jirkov::removeDict(BST< std::string, dictionary >& dicts, std::istream& in, std::ostream& out)
+void jirkov::removeDict(std::map< std::string, dictionary >& dicts, std::istream& in, std::ostream& out)
 {
+  auto warningMes = std::bind(warning, std::placeholders::_1, "Dictionary does not exist\n");
   std::string dictName;
   in >> dictName;
-  if (dicts.find(dictName) == dicts.cend())
+  if (dicts.find(dictName) == dicts.end())
   {
-    warningDict(out);
+    warningMes(out);
     return;
   }
   dicts.erase(dictName);
 }
 
-void jirkov::printDict(const BST< std::string, dictionary >& dicts, std::istream& in, std::ostream& out)
+void jirkov::printDict(const std::map< std::string, dictionary >& dicts, std::istream& in, std::ostream& out)
 {
+  auto warningMes = std::bind(warning, std::placeholders::_1, "Dictionary does not exist\n");
   std::string dictName;
   in >> dictName;
-  if (dicts.constFind(dictName) == dicts.cend())
+  if (dicts.find(dictName) == dicts.end())
   {
-    warningDict(out);
+    warningMes(out);
     return;
   }
-  dictionary dictToPrint = dicts.constFind(dictName)->second;
+  dictionary dictToPrint = dicts.find(dictName)->second;
   out << dictName << "\n";
-  if (dictToPrint.isEmpty())
+  if (dictToPrint.empty())
   {
     return;
   }
@@ -185,137 +204,122 @@ void jirkov::printDict(const BST< std::string, dictionary >& dicts, std::istream
   }
 }
 
-void jirkov::sortDict(BST< std::string, dictionary >& dicts, std::istream& input, std::ostream& output)
+void jirkov::sortDict(std::map< std::string, dictionary >& dicts, std::istream& input, std::ostream& output)
 {
   using namespace std::placeholders;
-  using func = std::function< void(BST< std::string, dictionary >&, std::istream&, std::ostream&) >;
-  BST< std::string, func > cmdSort;
-  cmdSort.insert("frequency", std::bind(jirkov::sortByFrequency, _1, _2, _3));
-  cmdSort.insert("alphabet", std::bind(jirkov::sortByAlphabet, _1, _2, _3));
+  using func = std::function< void(std::map< std::string, dictionary >&, std::istream&, std::ostream&) >;
+  std::map< std::string, func > cmdSort;
+  cmdSort["frequency"] = std::bind(jirkov::sortByFrequency, _1, _2, _3);
+  cmdSort["alphabet"] = std::bind(jirkov::sortByAlphabet, _1, _2, _3);
+  auto warningMes = std::bind(warning, std::placeholders::_1, "Dictionary does not exist\n");
   std::string sortType;
   input >> sortType;
-  if (dicts.isEmpty())
+  try
   {
-    warningDict(output);
-  }
-  else
-  {
-    cmdSort.at(sortType).second(dicts, input, output);
-  }
-}
-
-void jirkov::sortByFrequency(BST< std::string, dictionary >& dicts, std::istream& in, std::ostream& out)
-{
-  std::string dictName;
-  in >> dictName;
-  if (dicts.find(dictName) == dicts.cend())
-  {
-    warningDict(out);
-    return;
-  }
-  dictionary dictToSort = dicts.find(dictName)->second;
-  if (dictToSort.isEmpty() || std::isdigit(dictToSort.begin()->first[0]))
-  {
-    return;
-  }
-  out << dictName << "\n";
-  BST< std::string, std::string > temp;
-  for (auto it = dictToSort.begin(); it != dictToSort.end(); it++)
-  {
-    temp.insert(it->first, it->second);
-  }
-  size_t i = 0;
-  while (i != temp.getSize())
-  {
-    auto maxIter = temp.begin();
-    for (auto iter = temp.begin(); iter != temp.end(); ++iter)
+    if (dicts.empty())
     {
-      if (std::stoll(iter->second) > std::stoll(maxIter->second) && std::stoll(iter->second) != 0)
-      {
-        maxIter = iter;
-      }
+      warningMes(output);
     }
-    out << maxIter->first << ": " << maxIter->second << "\n";
-    temp.at(maxIter->first).second = "0";
-    i++;
+    else
+    {
+      cmdSort.at(sortType)(dicts, input, output);
+    }
+  }
+  catch (const std::out_of_range& e)
+  {
+    warningMes(output);
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
   }
 }
 
-void jirkov::sortByAlphabet(BST< std::string, dictionary >& dicts, std::istream& in, std::ostream& out)
+void jirkov::sortByFrequency(std::map< std::string, dictionary >& dicts, std::istream& in, std::ostream& out)
 {
+  auto warningMes = std::bind(warning, std::placeholders::_1, "Dictionary does not exist\n");
   std::string dictName;
   in >> dictName;
-  if (dicts.find(dictName) == dicts.cend())
+  if (dicts.find(dictName) == dicts.end())
   {
-    warningDict(out);
+    warningMes(out);
     return;
   }
   dictionary dictToSort = dicts.find(dictName)->second;
-  if (dictToSort.isEmpty() || !std::isdigit(dictToSort.begin()->first[0]))
+  if (dictToSort.empty() || std::isdigit(dictToSort.begin()->first[0]))
   {
-    out << "The dictionary is already sorted\n";
     return;
   }
-  BST< std::string, std::string > temp;
-  for (auto it = dictToSort.begin(); it != dictToSort.end(); it++)
+  std::multimap< std::string, std::string > temp;
+  for (const auto& pair : dictToSort)
   {
-    temp.insert(it->second, it->first);
+    temp.emplace(pair.second, pair.first);
   }
-  out << dictName << "\n";
-  for (auto it = temp.begin(); it != temp.end(); it++)
+  dicts.erase(dictName);
+  dicts.emplace(dictName, temp);
+}
+
+void jirkov::sortByAlphabet(std::map< std::string, dictionary >& dicts, std::istream& in, std::ostream& out)
+{
+  auto warningMes = std::bind(warning, std::placeholders::_1, "Dictionary does not exist\n");
+  std::string dictName;
+  in >> dictName;
+  if (dicts.find(dictName) == dicts.end())
+  {
+    warningMes(out);
+    return;
+  }
+  dictionary dictToSort = dicts.find(dictName)->second;
+  if (dictToSort.empty() || !std::isdigit(dictToSort.begin()->first[0]))
+  {
+    return;
+  }
+  std::multimap< std::string, std::string > temp;
+  for (const auto& pair : dictToSort)
+  {
+    temp.emplace(pair.second, pair.first);
+  }
+  dicts.erase(dictName);
+  dicts.emplace(dictName, temp);
+}
+
+void jirkov::printTop(std::map< std::string, dictionary >& dicts, std::istream& in, std::ostream& out)
+{
+  auto warningMes = std::bind(warning, std::placeholders::_1, "Dictionary does not exist\n");
+  std::string dictName;
+  in >> dictName;
+  if (dicts.find(dictName) == dicts.end())
+  {
+    warningMes(out);
+    return;
+  }
+  dictionary dictToSort = dicts.find(dictName)->second;
+  if (dictToSort.empty())
+  {
+    return;
+  }
+  else if (std::isdigit(dictToSort.begin()->first[0]))
+  {
+    auto it = dictToSort.rbegin();
+    for (int i = 0; i < 3; ++i)
+    {
+      out << it->first << ": " << it->second << "\n";
+      ++it;
+    }
+    return;
+  }
+  std::multimap< std::string, std::string > temp;
+  for (const auto& pair : dictToSort)
+  {
+    temp.emplace(pair.second, pair.first);
+  }
+  auto it = temp.rbegin();
+  for (int i = 0; i < 3; ++i)
   {
     out << it->first << ": " << it->second << "\n";
+    ++it;
   }
 }
 
-void jirkov::printTop(BST< std::string, dictionary >& dicts, std::istream& in, std::ostream& out)
+void jirkov::warning(std::ostream& output, const std::string& mes)
 {
-  std::string dictName;
-  in >> dictName;
-  if (dicts.find(dictName) == dicts.cend())
-  {
-    warningDict(out);
-    return;
-  }
-  dictionary dictToSort = dicts.find(dictName)->second;
-  BST< std::string, std::string > temp;
-  for (auto it = dictToSort.begin(); it != dictToSort.end(); it++)
-  {
-    temp.insert(it->first, it->second);
-  }
-  size_t i = 0;
-  while (i < 3)
-  {
-    auto maxIter = temp.begin();
-    for (auto iter = temp.begin(); iter != temp.end(); ++iter)
-    {
-      if (std::stoll(iter->second) > std::stoll(maxIter->second) && std::stoll(iter->second) != 0)
-      {
-        maxIter = iter;
-      }
-    }
-    out << maxIter->first << ": " << maxIter->second << "\n";
-    temp.at(maxIter->first).second = "0";
-    i++;
-  }
-}
-
-void jirkov::warningDict(std::ostream& output)
-{
-  output << "Dictionary does not exist\n";
-}
-
-void jirkov::warningElem(std::ostream& output)
-{
-  output << "Element does not exist\n";
-}
-
-void jirkov::warningFile(std::ostream& output)
-{
-  output << "File does not exist\n";
-}
-
-void jirkov::warningInvCom(std::ostream& output)
-{
-  output << "<INVALID COMMAND>\n";
+  output << mes;
 }
